b4ef30c30afd9a72895c34943ca7803822cceb6d
Sergey <sclef4@mail.ru>
2015-11-08 14:50:45 +0200

    q

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,47 @@
+import java.util.Scanner;
+
 /**
  * Created by Sergey on 07.11.2015.
  */
 public class Main {
     public static void main(String[] args) {
         Methods m = new Methods();
-        m.simplePasswordGenerator(15);
-        m.complexPasswordGenerator(15);
-        m.mediumPasswordGenerator(15);
+
+        System.out.println("Choose the item:");
+        System.out.println("1. Generate simple password.");
+        System.out.println("2. Generate medium difficulty password.");
+        System.out.println("3. Generate complex difficulty password.");
+
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1: {
+                System.out.println("Enter the length of password : ");
+                Scanner input2 = new Scanner(System.in);
+                int length = input2.nextInt();
+                m.simplePasswordGenerator(length);
+            }
+            break;
+
+            case 2: {
+                System.out.println("Enter the length of password : ");
+                Scanner input2 = new Scanner(System.in);
+                int length = input2.nextInt();
+                m.mediumPasswordGenerator(length);
+            }
+            break;
+
+            case 3: {
+                System.out.println("Enter the length of password : ");
+                Scanner input2 = new Scanner(System.in);
+                int length = input2.nextInt();
+                m.complexPasswordGenerator(length);
+            }
+            break;
+
+            default:
+                System.out.println("You entered wrong number.");
+                break;
+        }
     }
 }
